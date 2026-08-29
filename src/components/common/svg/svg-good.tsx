59941931a07d75b60b6ec1398dd340404184f041
Lorenzo <Lorenzo.travail@gmail.COM>
2026-08-29 21:02:12 +0200

    Restore health scan panel curve

--- src/components/common/svg/svg-good.tsx
+++ src/components/common/svg/svg-good.tsx
@@ -4,16 +4,16 @@ import Svg, { Path, Defs, LinearGradient, Stop } from 'react-native-svg';
 const HealthGood = () => (
   <Svg width="415" height="426.6" viewBox="0 0 430 441" fill="none">
     <Path
-      d="M0 -2H430V642C430 642 401 670 213.5 671C26 672 0 642 0 642V-2Z"
+      d="M0 -2H430V421.895C430 421.895 401 440.326 213.5 440.984C26 441.642 0 421.895 0 421.895V-2Z"
       fill="url(#paint0_linear_1_275)"
     />
     <Defs>
       <LinearGradient
         id="paint0_linear_1_275"
         x1="215"
-        y1="671"
-        x2="62.5"
-        y2="101.5"
+        y1="440.984"
+        x2="146.324"
+        y2="51.3514"
         gradientUnits="userSpaceOnUse">
         <Stop stopColor="#00421A" />
         <Stop offset="1" stopColor="#0B5E2C" />
